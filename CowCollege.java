3c6ad1f9e2cf44c696c140d37b2a1991cf8877b4
Piyush Acharya <acharyapiyush1@outlook.com>
2022-12-17 16:06:49 -0800

    Remove unused imports

--- CowCollege.java
+++ CowCollege.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CowCollege {
     public static void main(String[] args) {
